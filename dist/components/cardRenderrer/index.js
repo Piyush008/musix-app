6defe6c12f776a8bfdfecd186d3c6aa5290b77f4
Piyush008 <Piyush008@users.noreply.github.com>
2022-01-25 10:21:26 +0000

    deploy: 63191a3291fd7e9e15980f79df78bf6a0d8ce7ab

--- dist/components/cardRenderrer/index.js
+++ dist/components/cardRenderrer/index.js
@@ -1,20 +1,37 @@
 import React from "../../../_snowpack/pkg/react.js";
 import {Grid} from "../../../_snowpack/pkg/@chakra-ui/react.js";
+import AgentDetect from "../util/AgentDetect.js";
 function CardRenderer({
   minCardWidth,
   children,
   noOfRows,
   autoRows,
+  noOfChildren,
   ...otherProps
 }) {
-  return /* @__PURE__ */ React.createElement(Grid, {
-    templateColumns: `repeat(auto-fill,minmax(${minCardWidth},1fr))`,
-    templateRows: `repeat(${noOfRows},1fr)`,
-    autoRows: otherProps.overflowY != "hidden" ? void 0 : autoRows,
-    rowGap: "0.7rem",
-    columnGap: "0.7rem",
-    ...otherProps
-  }, children);
+  return /* @__PURE__ */ React.createElement(AgentDetect, {
+    desktopComponent: /* @__PURE__ */ React.createElement(Grid, {
+      templateColumns: `repeat(auto-fill,minmax(${minCardWidth},1fr))`,
+      templateRows: `repeat(${noOfRows},1fr)`,
+      autoRows: otherProps.overflowY != "hidden" ? void 0 : autoRows,
+      rowGap: "0.7rem",
+      columnGap: "0.7rem",
+      ...otherProps
+    }, children),
+    mobileComponent: /* @__PURE__ */ React.createElement(Grid, {
+      templateColumns: `repeat(${noOfChildren},40vw)`,
+      templateRows: `repeat(${noOfRows},1fr)`,
+      autoRows: otherProps.overflowY != "hidden" ? void 0 : autoRows,
+      rowGap: "0.7rem",
+      columnGap: "0.7rem",
+      css: {
+        "&::-webkit-scrollbar": {
+          display: "none"
+        }
+      },
+      ...otherProps
+    }, children)
+  });
 }
 CardRenderer.defaultProps = {
   minCardWidth: "180px",
@@ -22,6 +39,7 @@ CardRenderer.defaultProps = {
   columnGap: "0.7rem",
   overflowX: "auto",
   overflowY: "hidden",
-  autoRows: 0
+  autoRows: 0,
+  noOfChildren: 6
 };
 export default CardRenderer;
