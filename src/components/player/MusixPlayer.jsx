afa43dd31b376d48c1e579b3dabdb340522b82fd
Piyush008 <piyushjyotisahu@gmail.com>
2022-02-04 00:54:07 +0530

    made changes to music player

--- src/components/player/MusixPlayer.jsx
+++ src/components/player/MusixPlayer.jsx
@@ -1,11 +1,23 @@
-import { Box, HStack } from "@chakra-ui/react";
+import {
+  Box,
+  HStack,
+  IconButton,
+  Slider,
+  SliderFilledTrack,
+  SliderThumb,
+  SliderTrack,
+  VStack,
+} from "@chakra-ui/react";
 import useAgent from "../../hooks/useAgent.js";
 import { pxToAll, pxToRem } from "../../utils/theme.utils.js";
+import { FaPause, FaPlay } from "react-icons/fa";
 
 export default function MusixPlayer() {
   const isMobile = useAgent();
+  const secondsToMins = (sec) =>
+    `${Math.floor(sec / 60)}:${("0" + (sec % 60)).slice(-2)}`;
   return (
-    <HStack
+    <VStack
       justify={"space-evenly"}
       height={pxToAll(100)}
       pos={"fixed"}
@@ -14,6 +26,54 @@ export default function MusixPlayer() {
       bg={"brand.secondary"}
       zIndex={"1"}
       boxShadow={`0 -5px 25px rgba(0,0,0,0.2)`}
-    ></HStack>
+    >
+      <Slider aria-label="slider-ex-2" colorScheme="pink" defaultValue={50}>
+        <SliderTrack>
+          <SliderFilledTrack />
+        </SliderTrack>
+        <SliderThumb />
+      </Slider>
+      <HStack
+        w="100%"
+        justifyContent={"space-between"}
+        paddingLeft={"4"}
+        paddingRight={"4"}
+        paddingBottom={"2"}
+      >
+        <GoogleLogout />
+        <Box
+          borderRadius={"1rem"}
+          borderWidth={"thin"}
+          p="1.5"
+          fontSize={pxToAll(18)}
+          boxShadow={"inset 0 0 20px black"}
+          borderColor={"blackAlpha.200"}
+        >
+          {secondsToMins(0)}
+        </Box>
+        <Box>
+          <IconButton
+            fontSize={`${pxToRem(24)} !important`}
+            paddingTop={`${pxToRem(18)} !important`}
+            paddingBottom={`${pxToRem(18)} !important`}
+            paddingInlineStart={`${pxToRem(18)} !important`}
+            paddingInlineEnd={`${pxToRem(18)} !important`}
+            borderRadius="100%"
+            isRound={true}
+            icon={<FaPause />}
+          />
+        </Box>
+        <Box
+          borderRadius={"1rem"}
+          borderWidth={"thin"}
+          p="1.5"
+          fontSize={pxToAll(18)}
+          boxShadow={"inset 0 0 20px black"}
+          borderColor={"blackAlpha.200"}
+        >
+          {secondsToMins(500)}
+        </Box>
+      </HStack>
+    </VStack>
   );
 }
